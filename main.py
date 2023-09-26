d1b09a49e0eba64b4770f5aad16025e67e03192d
reamaestas <reamaestas@yahoo.com>
2023-09-26 21:32:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# ---- Loops, Conditionals, and Strings! ----
+
+word = 'bag'
+
+# 1. Set up a loop to iterate through the string of lowercase vowels, 'aeiou'.
+
+
+
+# 2 & 3. Inside the loop, create and print a new string from 'word', but with a different vowel. Use the replace() method.
+
+
+
+
+
+# 4. After you have your code working, try other words besides 'bag'.
